3394ede9c87678eeebf7783c4c9080c75f6f578f
Tadiwa Chipolopolo <tadiwanashenicckky@gmail.com>
2024-07-13 09:13:11 +0200

    updated navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import {
+  AppBar,
+  Box,
+  InputBase,
+  Link,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+
 function App() {
+  const MenuItems = [
+    { Name: "Home", Link: "/" },
+    { Name: "Products", Link: "#" },
+    { Name: "Portfolio", Link: "#" },
+    { Name: "Blog", Link: "#" },
+    { Name: "Contact Us", Link: "#" },
+  ];
+
   return (
     <>
-      <div>hello</div>
+      <AppBar sx={{ background: "black" }}>
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box sx={{ gap: "1rem", display: "flex" }}>
+            <Facebook />
+            <Instagram />
+            <Twitter />
+          </Box>
+          <Box sx={{ display: "flex", gap: "1rem" }}>
+            {MenuItems.map((items) => (
+              <Typography sx={{ cursor: "pointer" }}>{items.Name}</Typography>
+            ))}
+          </Box>
+          <Box sx={{ display: "flex", gap: "1rem" }}>
+            <InputBase placeholder="search" sx={{color:"white"}} />
+          </Box>
+        </Toolbar>
+      </AppBar>
     </>
   );
 }
