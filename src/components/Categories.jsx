be00c482044c276a4b7d214f9ca27e19a9690acd
Tadiwa Chipolopolo <tadiwanashenicckky@gmail.com>
2024-07-15 22:45:06 +0200

    generated a category component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,36 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, styled, Typography } from "@mui/material";
+
+const StyledBox = styled(Box)({
+  height: 200,
+  width: "100%",
+  cursor: "pointer",
+});
+
+const StyledTypography = styled(Typography)({
+  margin: "25% 50px 25% 50px",
+  backgroundColor: "white",
+  opacity: "0.8",
+});
 
 const Categories = () => {
   return (
     <>
       <Stack flexDirection={"row"} gap={4} position={"static"}>
-        <Box>box1</Box>
-        <Box>box2</Box>
-        <Box>box3</Box>
+        <StyledBox>
+          <StyledTypography variant="h3" align="center">
+            KIDS
+          </StyledTypography>
+        </StyledBox>
+        <StyledBox>
+          <StyledTypography variant="h3" align="center">
+            MEN
+          </StyledTypography>
+        </StyledBox>
+        <StyledBox>
+          <StyledTypography variant="h3" align="center">
+            WOMEN
+          </StyledTypography>
+        </StyledBox>
       </Stack>
     </>
   );
